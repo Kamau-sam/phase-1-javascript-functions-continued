7b716d4cedcfb49c60a5c752b73583af1def36c9
Kamau-sam <skamau.6411@gmail.com>
2024-03-20 08:42:19 +0300

    All test passed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+  return`This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(parent="*"){
+    return function (para1="special"){
+        return `You are ${parent}${para1}${parent}!`;
+    }
+
+}
